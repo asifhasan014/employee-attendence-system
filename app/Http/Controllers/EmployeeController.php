1785dfd2254a866cc4042d5a3f1a1c4baf0d554d
hasan07023@gmail.com <asifhasan014@gmail.com>
2021-05-07 22:03:08 +0600

    data is retrive

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -3,15 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Companie;
 
 class EmployeeController extends Controller
 {
     //
-    public function dashboard(){
+    public function dashboard()
+    {
         echo "this is dashboard";
     }
-    public function createEmployee(Request $request){
-        return "this is create employee controller";
+    public function createEmployee(Request $request)
+    {
+        $companie = Companie::where('company_code', $request->input('company_code'))->get();
+        return $companie;
     }
-    
 }
